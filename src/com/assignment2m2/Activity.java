2260ff8133bc2cd8bfb8a26e155c350677ce781e
FafeZuniga <70952484+FafeZuniga@users.noreply.github.com>
2020-10-13 20:27:03 +1100

    created setters in Activity class

--- src/com/assignment2m2/Activity.java
+++ src/com/assignment2m2/Activity.java
@@ -6,10 +6,28 @@ public class Activity {
     String description;
     int ticketsSold;
 
-    public Activity (String title, String description, int ticketsSold){
+    public Activity(String title, String description, int ticketsSold) {
         this.title = title;
         this.description = description;
-        this.ticketsSold =  ticketsSold;
+        this.ticketsSold = ticketsSold;
     }
+
+    public void displayDetails() {
+    System.out.println("Title: " + title);
+    System.out.println("Description: " + description);
+    System.out.println("Tickets Sold: " + ticketsSold);
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public void setTicketsSold(int ticketsSold){
+        ticketsSold = ticketsSold;
+    }
+
+
+
 
 }
